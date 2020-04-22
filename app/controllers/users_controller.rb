75a3d437c70b3830b5245357e8d77b65d1c38ace
idgm5 <idgm@pm.me>
2020-04-22 15:49:01 -0500

    Add form_tag, Edit function and views. Including PUT method for hidden input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,8 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit]
 end
