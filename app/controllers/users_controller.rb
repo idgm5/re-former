f95572cfe813e6d926f33f3919d2867c6688042b
idgm5 <idgm@pm.me>
2020-04-22 16:28:47 -0500

    Fix edit form, Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,9 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
 end
